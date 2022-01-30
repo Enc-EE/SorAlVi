618d7a1ea2b3dafc51dc4ddabc800a553eb354d3
Eric Eichhorn <ee.eric.eichhorn@gmail.com>
2022-01-30 13:41:54 +0100

    trigger build

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,6 @@ export default class App extends React.Component<{}, State> {
         value: this.firstAlgorithm,
         language: 'javascript',
       })
-      // this.monacoEditor.change
     }
     if (this.visualizerContainer) {
       console.log('creating canvas')
